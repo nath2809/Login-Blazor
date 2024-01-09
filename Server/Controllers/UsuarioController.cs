9355691c149e39081f458def6836c0c7d2bca80c
Nathalia Moncada <114375037+nath2809@users.noreply.github.com>
2024-01-08 19:42:43 -0500

    Add files via upload

---
+++ Server/Controllers/UsuarioController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Loginn.Shared;
+
+namespace Loginn.Server.Controllers //bASE DE DATOS 
+
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO login)
+        {
+            SesionDTO sesionDTO = new SesionDTO();
+
+            if (login.Correo == "admin@gmail.com" && login.Clave == "admin")
+            {
+                sesionDTO.Nombre = "admin";
+                sesionDTO.Correo = login.Correo;
+                sesionDTO.Rol = "Administrador";
+            }
+            else
+            {
+                sesionDTO.Nombre = "empleado";
+                sesionDTO.Correo = login.Correo;
+                sesionDTO.Rol = "Empleado";
+
+            }
+            return StatusCode(StatusCodes.Status200OK, sesionDTO);
+
+
+
+
+        }
+    }
+}
