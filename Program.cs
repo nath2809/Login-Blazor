9443275a6c3adbc48eb5b9e138aed8df4803ca5b
Nathalia Moncada <114375037+nath2809@users.noreply.github.com>
2024-01-08 19:44:59 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+using Loginn.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using Loginn.Client.Extensiones;
+
+
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredSessionStorage();
+builder.Services.AddScoped<AuthenticationStateProvider, AutentificacionExtension>();
+builder.Services.AddAuthorizationCore();
+
+
+
+
+await builder.Build().RunAsync();
